7ae80fa42fab251065d489fd73ad14cb6959ef6f
fizorg103 <fizorg103@gmail.com>
2019-07-07 18:27:20 +0700

    fixed IDEA errors (del new Double())

--- MyProjects/src/ru/academits/fedorov/shapes/Comporator/ShapePerimeterComporator.java
+++ MyProjects/src/ru/academits/fedorov/shapes/Comporator/ShapePerimeterComporator.java
@@ -8,8 +8,8 @@ public class ShapePerimeterComporator implements Comparator<Shape> {
 
     @Override
     public int compare(Shape shape1, Shape shape2) {
-        Double area1 = new Double(shape1.getPerimeter());
-        Double area2 = new Double(shape2.getPerimeter());
+        Double area1 = shape1.getPerimeter();
+        Double area2 = shape2.getPerimeter();
         return area1.compareTo(area2);
     }
 }
