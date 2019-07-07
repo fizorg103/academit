6713a1db8a869fa0de817c4612800778202f9433
fizorg103 <fizorg103@gmail.com>
2019-07-07 18:17:54 +0700

    create AreaShapeComporator

---
+++ MyProjects/src/ru/academits/fedorov/shapes/Comporator/ShapeAreaComporator.java
@@ -0,0 +1,15 @@
+package ru.academits.fedorov.shapes.Comporator;
+
+import ru.academits.fedorov.shapes.Shape;
+
+import java.util.Comparator;
+
+public class ShapeAreaComporator implements Comparator<Shape> {
+
+    @Override
+    public int compare(Shape shape1, Shape shape2) {
+        Float area1 = new Float(shape1.getArea());
+        Float area2 = new Float(shape2.getArea());
+        return area1.compareTo(area2);
+    }
+}
