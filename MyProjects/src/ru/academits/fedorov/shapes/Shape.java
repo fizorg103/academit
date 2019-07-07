0a379bddea95f9796f3f62954423e6ea879254b2
fizorg103 <fizorg103@gmail.com>
2019-07-07 16:33:54 +0700

    create Shape

---
+++ MyProjects/src/ru/academits/fedorov/shapes/Shape.java
@@ -0,0 +1,23 @@
+package ru.academits.fedorov.shapes;
+
+public class Shape {
+    public double getWidth() {
+        return 0;
+    }
+
+    public double getHeight() {
+        return 0;
+    }
+
+    public double getArea() {
+        return 0;
+    }
+
+    public double getPerimeter() {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
--- MyProjects/src/ru/academits/fedorov/shapes/Shapes.java
+++
@@ -1,7 +0,0 @@
-package ru.academits.fedorov.shapes;
-
-public class Shapes {
-
-    public static void main(String[] args) {
-    }
-}
