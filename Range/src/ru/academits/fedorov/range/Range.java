d6a7ef5c707664d55a09df8b3b977a1880c59238
fizorg103 <fizorg103@gmail.com>
2019-07-08 11:24:05 +0700

    ready to review

--- Range/src/ru/academits/fedorov/range/Range.java
+++ Range/src/ru/academits/fedorov/range/Range.java
@@ -31,42 +31,46 @@ public class Range {
     }
 
     public Range getIntersection(Range range) {
-        double to = range.getTo();
-        double from = range.getFrom();
-        if (from < this.from) {
-            if (to < this.from) {
-                return null;
-            }
-            if (to < this.to) {
-                return new Range(this.from, to);
-            }
-            return new Range(this.from, this.to);
+        double from = Math.max(this.from, range.from);
+        double to = Math.min(this.to, range.to);
+        if (from < to) {
+            return new Range(from, to);
         }
-
-        if (from < this.to) {
-            if (to < this.to) {
-                return new Range(from, to);
-            }
-            return new Range(from, this.to);
-        }
-
         return null;
     }
 
     public Range[] getUnion(Range range) {
-        double from = range.getFrom();
-        double to = range.getTo();
-        if (this.getIntersection(range) != null) {
-            from = Math.min(this.from, from);
-            to = Math.max(this.to, to);
-            return new Range[]{new Range(from, to)};
+        double from = range.from;
+        double to = range.to;
+
+        double fromMax;
+        double fromMin;
+        if (from < this.from) {
+            fromMax = this.from;
+            fromMin = from;
+        } else {
+            fromMax = from;
+            fromMin = this.from;
+        }
+        double toMax;
+        double toMin;
+        if (to < this.to) {
+            toMax = this.to;
+            toMin = to;
+        } else {
+            toMax = to;
+            toMin = this.to;
+        }
+
+        if (fromMax < toMin) {
+            return new Range[]{new Range(fromMin, toMax)};
         }
         return new Range[]{new Range(this.from, this.to), new Range(from, to)};
     }
 
     public Range[] getComplement(Range range) {
-        double from = range.getFrom();
-        double to = range.getTo();
+        double from = range.from;
+        double to = range.to;
 
         if (from < this.from) {
             if (to < this.from) {
