6a43ec01fb3df14380b4c70ce2ea1926232f9fb6
fizorg103 <fizorg103@gmail.com>
2019-07-14 22:53:16 +0700

    isEquals for double

--- Shape/src/ru/academits/fedorov/shape/Shape.java
+++ Shape/src/ru/academits/fedorov/shape/Shape.java
@@ -16,4 +16,9 @@ public class Shape {
     public double getPerimeter() {
         return 0;
     }
+
+    protected static boolean isEquals(double number1, double number2) {
+        double epsilon = 1e-23;
+        return (Math.abs(number1 - number2) < epsilon);
+    }
 }
