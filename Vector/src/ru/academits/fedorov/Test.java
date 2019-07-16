d01554a3c4d6efb1c4173091c70b7efa17f80402
fizorg103 <fizorg103@gmail.com>
2019-07-16 21:01:20 +0700

    ready to review

---
+++ Vector/src/ru/academits/fedorov/Test.java
@@ -0,0 +1,42 @@
+package ru.academits.fedorov;
+
+import ru.academits.fedorov.vector.Vector;
+
+import java.util.Locale;
+
+public class Test {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        double[] array1 = {1, 2, 3, 4, 5};
+        double[] array2 = {4, 7, 2, 3};
+
+        Vector vector1 = new Vector(array1);
+        Vector vector1Copy = new Vector(vector1);
+        Vector vector2 = new Vector(array2);
+
+        System.out.println("Вектор 1: " + vector1.toString() + " размер: " + vector1.getSize() + " длина: " + vector1.getLength());
+        System.out.println("Вектор 2: " + vector2.toString() + " размер: " + vector2.getSize() + " длина: " + vector2.getLength());
+
+        vector1.add(vector2);
+        System.out.println("Прибавим к Вектору 1 Вектор 2: " + vector1.toString());
+
+        vector1.scalarMultiplication(5);
+        System.out.println("Умножим на 5" + vector1.toString());
+
+        vector1.subtract(vector2);
+        Vector vector3 = new Vector(vector2);
+        System.out.println("Отнимем Вектор 2: " + vector3.toString());
+        System.out.print(vector2 == vector3);
+        System.out.print(" ");
+        System.out.println(vector3.equals(vector2));
+
+        Vector vector4 = Vector.add(vector1Copy, vector2);
+        System.out.println(vector4.toString());
+
+        Vector vector5 = Vector.subtract(vector1Copy, vector2);
+        System.out.println(vector5.toString());
+
+        double dotResult = Vector.dot(vector1Copy, vector2);
+        System.out.println(dotResult);
+    }
+}
--- Vector/src/ru/academits/fedorov/vector/Vector.java
+++ Vector/src/ru/academits/fedorov/vector/Vector.java
@@ -6,8 +6,10 @@ public class Vector {
     private double[] values;
 
     public Vector(int n) {
+        if (n <= 0) {
+            throw new IllegalArgumentException("Argument N <= 0.");
+        }
         values = new double[n];
-
         for (int i = 0; i < n - 1; ++i) {
             values[i] = 0;
         }
@@ -25,6 +27,93 @@ public class Vector {
         this.values = Arrays.copyOf(values, n);
     }
 
+    public int getSize() {
+        return values.length;
+    }
+
+    public void add(Vector vector) {
+        int minIndex = Math.min(values.length, vector.values.length);
+        for (int i = 0; i < minIndex; ++i) {
+            if (i < vector.values.length) {
+                values[i] += vector.values[i];
+            }
+        }
+    }
+
+    public void subtract(Vector vector) {
+        int minIndex = Math.min(values.length, vector.values.length);
+        for (int i = 0; i < minIndex; ++i) {
+            if (i < vector.values.length) {
+                values[i] -= vector.values[i];
+            }
+        }
+    }
+
+    public void scalarMultiplication(double alpha) {
+        for (int i = 0; i < values.length; ++i) {
+            values[i] *= alpha;
+        }
+    }
+
+    public void expand() {
+        for (int i = 0; i < values.length; ++i) {
+            values[i] *= -1;
+        }
+    }
+
+    public double getLength() {
+        double length = 0;
+        for (double value : values) {
+            length += Math.pow(value, 2);
+        }
+        return Math.sqrt(length);
+    }
+
+    public double getValue(int index) {
+        if (index < 0 || index > values.length - 1) {
+            throw new IllegalArgumentException("index out of range");
+        } else {
+            return values[index];
+        }
+    }
+
+    public void setValue(int index, double value) {
+        if (index < 0 || index > values.length - 1) {
+            throw new IllegalArgumentException("index out of range");
+        } else {
+            values[index] = value;
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(values);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (o == null || this.getClass() != o.getClass()) {
+            return false;
+        }
+
+        Vector vector = (Vector) o;
+
+        if (values.length != vector.values.length) {
+            return false;
+        }
+
+        for (int i = 0; i < values.length; ++i) {
+            if (values[i] != vector.values[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     @Override
     public String toString() {
         StringBuilder result = new StringBuilder("{");
@@ -38,5 +127,41 @@ public class Vector {
         }
         result.append("}");
         return result.toString();
+    }
+
+    public static Vector add(Vector vector1, Vector vector2) {
+        Vector vectorRes;
+        if (vector1.values.length > vector2.values.length) {
+            vectorRes = new Vector(vector1);
+            vectorRes.add(vector2);
+            return vectorRes;
+        }
+
+        vectorRes = new Vector(vector2);
+        vectorRes.add(vector1);
+        return vectorRes;
+}
+
+    public static Vector subtract(Vector vector1, Vector vector2) {
+        Vector vectorRes;
+        if (vector1.values.length > vector2.values.length){
+            vectorRes = new Vector(vector1);
+            vectorRes.subtract(vector2);
+        }
+
+        vectorRes = new Vector(vector2);
+        vectorRes.expand();
+        vectorRes.add(vector1);
+
+        return vectorRes;
+    }
+
+    public static double dot(Vector vector1, Vector vector2) {
+        int minSize = Math.min(vector1.values.length, vector2.values.length);
+        double res = 0;
+        for (int i = 0; i < minSize; ++i) {
+            res += vector1.values[i] * vector2.values[i];
+        }
+        return res;
     }
 }
