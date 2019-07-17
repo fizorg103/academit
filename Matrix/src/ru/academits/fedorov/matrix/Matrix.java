c5ab6d497618799954c45adaac6ab1587a669970
fizorg103 <fizorg103@gmail.com>
2019-07-17 21:52:03 +0700

    scalarMultiplication transpose getDeterminant

--- Matrix/src/ru/academits/fedorov/matrix/Matrix.java
+++ Matrix/src/ru/academits/fedorov/matrix/Matrix.java
@@ -2,6 +2,8 @@ package ru.academits.fedorov.matrix;
 
 import ru.academits.fedorov.vector.Vector;
 
+import java.util.Arrays;
+
 public class Matrix {
     private Vector[] vectors;
 
@@ -63,12 +65,12 @@ public class Matrix {
         return vectors[index];
     }
 
-    public void setLine(int index, Vector vector){
+    public void setLine(int index, Vector vector) {
         if (index < 0 || index > vectors.length - 1) {
             throw new IllegalArgumentException("Out of range");
         }
 
-        vectors[index] = vector;
+        vectors[index] = new Vector(vectors[0].getSize(), vector.getValues());
     }
 
     public Vector getColumn(int index) {
@@ -83,16 +85,71 @@ public class Matrix {
         return vector;
     }
 
+    public void transpose() {
+        int m = vectors[0].getSize();
+        Vector[] vectorsT = new Vector[m];
+
+        for (int i = 0; i < m; ++i) {
+            vectorsT[i] = this.getColumn(i);
+        }
+
+        vectors = vectorsT;
+    }
+
+    public void scalarMultiplication(double alpha) {
+        for (int i = 0; i < vectors.length; ++i) {
+            vectors[i].scalarMultiplication(alpha);
+        }
+    }
+
+    public double getDeterminant() {
+        if (vectors.length != vectors[0].getSize()) {
+            throw new IllegalArgumentException("M != N. The Matrix does not have determinant.");
+        }
+
+        Vector[] vectors = new Vector[this.vectors.length];
+        for (int i = 0; i < vectors.length; ++i) {
+            vectors[i] = new Vector(this.vectors[i]);
+        }
+
+        double res = 1;
+        for (int i = 0; i < vectors.length - 1; ++i) {
+            for (int j = i + 1; j < vectors.length; ++j) {
+                if (vectors[j].getValue(i) == 0) {
+                    continue;
+                }
+                double alpha = -vectors[i].getValue(i) / vectors[j].getValue(i);
+                res /= alpha;
+                vectors[j].scalarMultiplication(alpha);
+                vectors[j].add(vectors[i]);
+
+                if (i == j - 1) {
+                    if (vectors[j].getValue(j) == 0) {
+                        return 0;
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < vectors.length; ++i) {
+            res *= vectors[i].getValue(i);
+        }
+        return res;
+    }
+
     @Override
     public String toString() { // TODO Передалать после Vector review
         StringBuilder result = new StringBuilder("{");
 
         for (int i = 0; i < vectors.length; ++i) {
+            if (i != 0) {
+                result.append(" ");
+            }
             result.append(vectors[i].toString());
             if (i == vectors.length - 1) {
                 break;
             }
-            result.append(", ");
+            result.append(", " + System.lineSeparator());
         }
         result.append("}");
         return result.toString();
