98770671cda927893952b715a12650efe22b75ff
fizorg103 <fizorg103@gmail.com>
2019-07-17 22:46:36 +0700

    test update

--- Matrix/src/ru/academits/fedorov/Test.java
+++ Matrix/src/ru/academits/fedorov/Test.java
@@ -3,18 +3,11 @@ package ru.academits.fedorov;
 import ru.academits.fedorov.matrix.Matrix;
 import ru.academits.fedorov.vector.Vector;
 
-import java.util.Arrays;
 import java.util.Locale;
 
 public class Test {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
-
-
-        double[][] array = {{1, 2, 3, 4}, {3, 4, 5, 7}};
-
-
-        System.out.println(Arrays.toString(array[0]));
 
         Vector[] vectors1 = {new Vector(new double[]{1, 2, 3, 4, 5}),
                             new Vector(new double[]{6, 7, 8, 9, 10}),
@@ -29,9 +22,7 @@ public class Test {
                             new Vector(new double[]{1, 3})};
 
         Matrix matrix2 = new Matrix(vectors2);
-
         Matrix matrixRes = Matrix.dot(matrix1,matrix2);
-
         System.out.println(matrixRes.toString());
     }
 }
