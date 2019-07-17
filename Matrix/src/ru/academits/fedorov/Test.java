ce24d6705a2502a1140d72bc9ac99914aa7aa845
fizorg103 <fizorg103@gmail.com>
2019-07-17 22:46:04 +0700

    test update

--- Matrix/src/ru/academits/fedorov/Test.java
+++ Matrix/src/ru/academits/fedorov/Test.java
@@ -1,10 +1,37 @@
 package ru.academits.fedorov;
 
 import ru.academits.fedorov.matrix.Matrix;
+import ru.academits.fedorov.vector.Vector;
+
+import java.util.Arrays;
+import java.util.Locale;
 
 public class Test {
     public static void main(String[] args) {
-        Matrix matrix = new Matrix(2, 3);
-        System.out.println(matrix.toString());
+        Locale.setDefault(Locale.US);
+
+
+        double[][] array = {{1, 2, 3, 4}, {3, 4, 5, 7}};
+
+
+        System.out.println(Arrays.toString(array[0]));
+
+        Vector[] vectors1 = {new Vector(new double[]{1, 2, 3, 4, 5}),
+                            new Vector(new double[]{6, 7, 8, 9, 10}),
+                            new Vector(new double[]{1, 2, 4, 5, 8}),
+                            new Vector(new double[]{1, 3, 4, 5, 6})};
+        Matrix matrix1 = new Matrix(vectors1);
+
+        Vector[] vectors2 = {new Vector(new double[]{1, 2}),
+                            new Vector(new double[]{6, 7}),
+                            new Vector(new double[]{1, 2}),
+                            new Vector(new double[]{1, 3}),
+                            new Vector(new double[]{1, 3})};
+
+        Matrix matrix2 = new Matrix(vectors2);
+
+        Matrix matrixRes = Matrix.dot(matrix1,matrix2);
+
+        System.out.println(matrixRes.toString());
     }
 }
